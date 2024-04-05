0ada27da16c2886ab658e3d4d2ed39346e19bd43
riol05 <148192071+riol05@users.noreply.github.com>
2024-04-05 21:42:47 +0900

    초기 커밋.

---
+++ Assets/Scipts/Third_PersonCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Player;
+using UnityEngine;
+
+
+public class Third_PersonCamera : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject targetCinemachine;
+    private float cinemachineTargetYaw;
+    private Input input;
+
+    public bool LockCameraPosition = false;
+    private const float threshOld = 0.01f;
+    private void Awake()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("Main Camera");
+    }
+
+    private void Start()
+    {
+        cinemachineTargetYaw = targetCinemachine.transform.rotation.eulerAngles.y;
+
+    }
+
+    public void CameraRotation()
+    {
+        if(input.look.sqrMagnitude >= threshOld && LockCameraPosition)
+        {
+
+        }
+    }
+}
